515c596e318249754ff62c70f87d9f18e3277490
Giovanny Massuia <giovannycmassuia@gmail.com>
2020-05-24 10:57:06 -0300

    Testing fail badge circle ci

--- src/test/java/com/giovannymassuia/springbootcircleci/SpringBootCircleCiApplicationTests.java
+++ src/test/java/com/giovannymassuia/springbootcircleci/SpringBootCircleCiApplicationTests.java
@@ -3,11 +3,16 @@ package com.giovannymassuia.springbootcircleci;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class SpringBootCircleCiApplicationTests {
 
     @Test
     void contextLoads() {
+
+        fail("Testing fail badge circle ci");
+
     }
 
 }
